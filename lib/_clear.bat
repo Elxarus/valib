360b6438bf70b6dc2b490430652de17ddcab2a07
valex <valex@localhost>
2006-07-06 07:00:23 +0000

    valib lib: clear libc folders

--- lib/_clear.bat
+++ lib/_clear.bat
@@ -17,3 +17,16 @@ del release\*.ilk 2> nul
 del release\*.asm 2> nul
 del release\*.cod 2> nul
 del release\*.map 2> nul
+
+rmdir /s /q debug_libc 2> nul
+del release_libc\*.bsc 2> nul
+del release_libc\*.exp 2> nul
+del release_libc\*.idb 2> nul
+del release_libc\*.obj 2> nul
+del release_libc\*.pch 2> nul
+del release_libc\*.res 2> nul
+del release_libc\*.sbr 2> nul
+del release_libc\*.ilk 2> nul
+del release_libc\*.asm 2> nul
+del release_libc\*.cod 2> nul
+del release_libc\*.map 2> nul
