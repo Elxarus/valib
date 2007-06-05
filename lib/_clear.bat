be7b35f803a34d99b6a4446f328ec3a34465b0dd
valex <valex@localhost>
2007-06-05 00:50:08 +0000

    clear libc build trash

--- lib/_clear.bat
+++ lib/_clear.bat
@@ -12,6 +12,7 @@ del *.dep 2> nul
 
 rmdir /s /q perf  2> nul
 rmdir /s /q debug 2> nul
+rmdir /s /q debug_libc 2> nul
 del release\*.pdb 2> nul
 del release\*.bsc 2> nul
 del release\*.exp 2> nul
@@ -24,6 +25,18 @@ del release\*.sbr 2> nul
 del release\*.ilk 2> nul
 del release\*.asm 2> nul
 del release\*.map 2> nul
+del release_libc\*.pdb 2> nul
+del release_libc\*.bsc 2> nul
+del release_libc\*.exp 2> nul
+del release_libc\*.idb 2> nul
+del release_libc\*.lib 2> nul
+del release_libc\*.obj 2> nul
+del release_libc\*.pch 2> nul
+del release_libc\*.res 2> nul
+del release_libc\*.sbr 2> nul
+del release_libc\*.ilk 2> nul
+del release_libc\*.asm 2> nul
+del release_libc\*.map 2> nul
 
 rem -------------------------------------------------------
 rem msvc8
