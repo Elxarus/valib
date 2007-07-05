71e1eafc99932c128839ff732c283c8ab3dfb95d
valex <valex@localhost>
2007-07-05 02:27:59 +0000

    Utilities removed

--- _clear.bat
+++ _clear.bat
@@ -1,32 +1,7 @@
 @echo off
 
-echo ac3enc...
-cd ac3enc
-call _clear.bat
-cd ..
-
-echo bsconvert...
-cd bsconvert
-call _clear.bat
-cd ..
-
 echo lib...
 cd lib
-call _clear.bat
-cd ..
-
-echo mpeg_demux...
-cd mpeg_demux
-call _clear.bat
-cd ..
-
-echo noise...
-cd noise
-call _clear.bat
-cd ..
-
-echo spdifer...
-cd spdifer
 call _clear.bat
 cd ..
 
@@ -34,9 +9,3 @@ echo test...
 cd test
 call _clear.bat
 cd ..
-
-echo valdec...
-cd valdec
-call _clear.bat
-cd ..
-
