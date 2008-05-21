4eb84304d0fed195e993bcaefef4d3b41684a4d2
valex <valex@localhost>
2008-05-21 23:28:17 +0000

    kaiser.h * kaiser_a(n, df) to determine maximum attenuation for the given filter length * extend functions to work with a < 21 (rect window)

--- valib/dsp/kaiser.h
+++ valib/dsp/kaiser.h
@@ -13,6 +13,12 @@ Kaiser window functions
 
   returns alpha parameter of a kaiser window for a given stopband attenuation
 
+* kaiser_a(n, df)
+  n - window length
+  df - transition band width (normalized) 
+
+  returns maximum possible attenuation for given window length and bandwidth
+
 * kaiser_n(a, df)
   a - attenuation in dB
   df - transition band width (normalized)
@@ -29,6 +35,7 @@ Kaiser window functions
 ******************************************************************************/
 
 inline double kaiser_alpha(double a);
+inline double kaiser_a(int n, double df);
 inline int    kaiser_n(double a, double df);
 inline double kaiser_window(double i, int n, double alpha);
 
@@ -36,18 +43,28 @@ inline double kaiser_window(double i, int n, double alpha);
 
 inline double kaiser_alpha(double a)
 {
-  if (a <= 21) return 0;
+  if (a <= 21) return 0; // rectangle window
   if (a <= 50) return 0.5842 * pow(a - 21, 0.4) + 0.07886 * (a - 21);
   return 0.1102 * (a - 8.7);
 }
 
+inline double kaiser_a(int n, double df)
+{
+  if (double(n) * df <= 0.9) return 21; // rectangle window
+  return 14.36 * df * double(n) + 7.95;
+}
+
 inline int kaiser_n(double a, double df)
 {
-  return (int) ((a - 7.95) / (14.36 * df) + 1);
+  if (fabs(a) <= 21)
+    return int(0.9 / df); // rectangle window
+  else
+    return int((fabs(a) - 7.95) / (14.36 * df) + 1);
 }
 
 inline double kaiser_window(double i, int n, double alpha)
 {
+  if (alpha == 0.0) return 1.0; // rectangle window
   double n1 = n - 1;
   return dbesi0(alpha * sqrt(1 - 4*i*i / (n1 * n1))) / dbesi0(alpha);
 }
