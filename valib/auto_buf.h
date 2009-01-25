f32e4c59e62090e5c0e0c1f960722b871ee696e1
valex <valex@localhost>
2009-01-25 14:27:32 +0000

    * New AutoBuf class (DataBuf and SampleBuf are to be replaced)

---
+++ valib/auto_buf.h
@@ -0,0 +1,59 @@
+/*
+  Simple buffer helper class
+*/
+
+#ifndef VALIB_AUTO_BUF_H
+#define VALIB_AUTO_BUF_H
+
+#include <string.h>
+
+template <class T> class AutoBuf
+{
+private:
+  T *f_buf;
+  size_t f_size;
+
+  AutoBuf(const AutoBuf &);
+  AutoBuf &operator =(const AutoBuf &);
+
+public:
+  AutoBuf(): f_buf(0), f_size(0)
+  {}
+
+  AutoBuf(size_t size): f_buf(0), f_size(0)
+  {
+    allocate(size);
+  }
+
+  ~AutoBuf()
+  { 
+    drop();
+  }
+
+  inline T *allocate(size_t size)
+  {
+    drop();
+    f_buf = new T[size];
+    if (f_buf)
+      f_size = size;
+    return f_buf;
+  }
+
+  inline void drop()
+  {
+    safe_delete(f_buf);
+    f_size = 0;
+  }
+
+  inline void zero()
+  {
+    if (f_buf)
+      memset(f_buf, 0, sizeof(T) * f_size);
+  }
+
+  inline size_t size() { return f_size; }
+  inline T *data()     { return f_buf;  }
+  inline operator T*() { return f_buf;  }
+};
+
+#endif
