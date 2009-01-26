fecce7f429664bcc0d75f0b72c9550c4cedf9924
valex <valex@localhost>
2009-01-26 08:26:55 +0000

    * samples_t: get rid of unused members

--- valib/spk.h
+++ valib/spk.h
@@ -323,10 +323,6 @@ struct samples_t
 {
   sample_t *samples[NCHANNELS];
 
-  inline sample_t *&operator [](int ch)          { return samples[ch]; }
-  inline sample_t  *operator [](int ch) const    { return samples[ch]; }
-  inline sample_t *&operator [](size_t ch)       { return samples[ch]; }
-  inline sample_t  *operator [](size_t ch) const { return samples[ch]; }
   inline sample_t *&operator [](unsigned ch)       { return samples[ch]; }
   inline sample_t  *operator [](unsigned ch) const { return samples[ch]; }
 
