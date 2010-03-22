6974d49fadd1eeb53e399400daf12fb3ea3a57e1
valex <valex@localhost>
2010-03-22 07:28:03 +0300

    spk: remove xxxx_samples functions with offets.

--- valib/spk.cpp
+++ valib/spk.cpp
@@ -1,4 +1,4 @@
-#include <memory.h>
+#include <string.h>
 #include "spk.h"
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -203,55 +203,26 @@ void zero_samples(sample_t *s, size_t size)
 {
   memset(s, 0, size * sizeof(sample_t));
 }
-void zero_samples(sample_t *s, size_t offset, size_t size)
-{
-  memset(s + offset, 0, size * sizeof(sample_t));
-}
 void zero_samples(samples_t s, int nch, size_t size)
 {
   for (int ch = 0; ch < nch; ch++)
     memset(s[ch], 0, size * sizeof(sample_t));
 }
-void zero_samples(samples_t s, size_t offset, int nch, size_t size)
-{
-  for (int ch = 0; ch < nch; ch++)
-    memset(s[ch] + offset, 0, size * sizeof(sample_t));
-}
-
 void copy_samples(sample_t *dst, sample_t *src, size_t size)
 {
   memcpy(dst, src, size * sizeof(sample_t));
 }
-void copy_samples(sample_t *dst, sample_t *src, size_t src_offset, size_t size)
-{
-  memcpy(dst, src + src_offset, size * sizeof(sample_t));
-}
-void copy_samples(sample_t *dst, size_t dst_offset, sample_t *src, size_t size)
-{
-  memcpy(dst + dst_offset, src, size * sizeof(sample_t));
-}
-void copy_samples(sample_t *dst, size_t dst_offset, sample_t *src, size_t src_offset, size_t size)
-{
-  memcpy(dst + dst_offset, src + src_offset, size * sizeof(sample_t));
-}
-
 void copy_samples(samples_t dst, samples_t src, int nch, size_t size)
 {
   for (int ch = 0; ch < nch; ch++)
     copy_samples(dst[ch], src[ch], size);
 }
-void copy_samples(samples_t dst, samples_t src, size_t src_offset, int nch, size_t size)
+void move_samples(sample_t *dst, sample_t *src, size_t size)
 {
-  for (int ch = 0; ch < nch; ch++)
-    copy_samples(dst[ch], src[ch] + src_offset, size);
+  memmove(dst, src, size * sizeof(sample_t));
 }
-void copy_samples(samples_t dst, size_t dst_offset, samples_t src, int nch, size_t size)
+void move_samples(samples_t dst, samples_t src, int nch, size_t size)
 {
   for (int ch = 0; ch < nch; ch++)
-    copy_samples(dst[ch] + dst_offset, src[ch], size);
-}
-void copy_samples(samples_t dst, size_t dst_offset, samples_t src, size_t src_offset, int nch, size_t size)
-{
-  for (int ch = 0; ch < nch; ch++)
-    copy_samples(dst[ch] + dst_offset, src[ch] + src_offset, size);
+    move_samples(dst[ch], src[ch], size);
 }
