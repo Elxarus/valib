16011b2a1fab144a4d188aa424a0175a47b9a377
valex <valex@localhost>
2010-05-02 17:07:47 +0400

    SourceFilter2: Source2 interface

---
+++ valib/source/source_filter.h
@@ -0,0 +1,45 @@
+/*
+  SourceFilter
+  Combination of a source and a filter, acting like a source.
+*/
+
+#ifndef VALIB_SOURCE_FILTER_H
+#define VALIB_SOURCE_FILTER_H
+
+#include "../filter2.h"
+#include "../source.h"
+
+class SourceFilter2 : public Source2
+{
+protected:
+  Source2 *source;
+  Filter2 *filter;
+
+  bool is_new_stream;
+  bool format_change;
+  enum { state_empty, state_filter, state_new_stream } state;
+
+public:
+  SourceFilter2():
+  source(0), filter(0)
+  {}
+
+  SourceFilter2(Source2 *source_, Filter2 *filter_):
+  source(0), filter(0)
+  { set(source_, filter_); }
+
+  bool set(Source2 *source_, Filter2 *filter_);
+  void release();
+
+  Source2 *get_source() const { return source; }
+  Filter2 *get_filter() const { return filter; }
+
+  /////////////////////////////////////////////////////////
+  // Source interface
+
+  virtual bool get_chunk(Chunk2 &out);
+  virtual bool new_stream() const;
+  virtual Speakers get_output() const;
+};
+
+#endif
