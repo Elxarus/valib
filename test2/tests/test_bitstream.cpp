98be90d35bbaaf05b7d2ca0352e406b414acef67
valex <valex@localhost>
2010-05-28 01:34:46 +0400

    Bitstream test: conversion functions test

--- test2/tests/test_bitstream.cpp
+++ test2/tests/test_bitstream.cpp
@@ -12,6 +12,7 @@
 static const int seed = 4967205;
 static const size_t max_align = 64;
 static const size_t max_start_bit = 64;
+static const size_t block_size = 512*1024; // block size for conversion test
 
 static uint32_t get_uint32(uint8_t *buf, size_t start_bit)
 {
@@ -20,6 +21,36 @@ static uint32_t get_uint32(uint8_t *buf, size_t start_bit)
   uint8_t next_byte = *(buf + start_bit / 8 + 4);
   return (dword << shift) | (next_byte >> (8 - shift));
 }
+
+static const int bs_types[] =
+{
+  BITSTREAM_8,
+  BITSTREAM_16BE, BITSTREAM_16LE,
+  //BITSTREAM_32BE, BITSTREAM_32LE,
+  BITSTREAM_14BE, BITSTREAM_14LE
+};
+
+static bool is_14bit(int bs_type)
+{
+  return bs_type == BITSTREAM_14BE || bs_type == BITSTREAM_14LE;
+}
+
+
+static const char *bs_name(int bs_type)
+{
+  switch (bs_type)
+  {
+    case BITSTREAM_8:    return "byte";
+    case BITSTREAM_16BE: return "16be";
+    case BITSTREAM_16LE: return "16le";
+    case BITSTREAM_32BE: return "32be";
+    case BITSTREAM_32LE: return "32le";
+    case BITSTREAM_14BE: return "14be";
+    case BITSTREAM_14LE: return "14le";
+    default: return "??";
+  }
+}
+
 
 ///////////////////////////////////////////////////////////////////////////////
 // ReadBS
@@ -376,6 +407,138 @@ BOOST_AUTO_TEST_CASE(flush)
 
   uint32_t result = get_uint32(buf, 0);
   BOOST_CHECK_EQUAL(result, test);
+}
+
+///////////////////////////////////////////////////////////
+// 2-step and 3-step bitstream conversion test
+
+static void bs_convert_test(int bs_type1, int bs_type2, int bs_type3 = -1)
+{
+  bs_conv_t conv1 = 0, conv2 = 0, conv3 = 0;
+
+  conv1 = bs_conversion(bs_type1, bs_type2);
+  BOOST_REQUIRE(conv1 != 0);
+
+  if (bs_type3 == -1)
+  {
+    conv2 = bs_conversion(bs_type2, bs_type1);
+    BOOST_REQUIRE(conv2 != 0);
+  }
+  else
+  {
+    conv2 = bs_conversion(bs_type2, bs_type3);
+    conv3 = bs_conversion(bs_type3, bs_type1);
+    BOOST_REQUIRE(conv2 != 0);
+    BOOST_REQUIRE(conv3 != 0);
+    BOOST_TEST_MESSAGE("Conversion " << 
+      bs_name(bs_type1) << "-" << 
+      bs_name(bs_type2) << "-" << 
+      bs_name(bs_type3));
+  }
+
+  // Prepare buffers
+  Rawdata ref_buf(block_size);
+  Rawdata buf1(block_size);
+  Rawdata buf2(block_size);
+  RNG(seed).fill_raw(ref_buf, ref_buf.size());
+
+  size_t ref_size = ref_buf.size();
+
+  // Conversion to 14bit increases amount of data,
+  // therefore we must decrease the size of source data
+  if (!is_14bit(bs_type1) && (is_14bit(bs_type2) || is_14bit(bs_type3)))
+  {
+    ref_size /= 8;
+    ref_size *= 7;
+  }
+
+  // 14bit source must have even buffer size.
+  // To maintain buffer size unchanged it must be multiple of 8.
+  // Also we must clear 2 high bits for 14bit source stream.
+  if (is_14bit(bs_type1))
+  {
+    ref_size &= ~7;
+
+    size_t n = ref_size;
+    if (bs_type1 == BITSTREAM_14LE)
+      n++;
+
+    while (n > 1)
+    {
+      n -= 2;
+      ref_buf[n] &= 0x3f;
+    }
+  }
+
+  size_t size1, size2;
+
+  /////////////////////////////////////////////////////////
+  // Inplace conversion test
+
+  memcpy(buf1, ref_buf, ref_size);
+  size1 = ref_size;
+
+  size1 = conv1(buf1, size1, buf1);
+  size1 = conv2(buf1, size1, buf1);
+  if (conv3)
+    size1 = conv3(buf1, size1, buf1);
+
+  BOOST_CHECK_EQUAL(size1, ref_size);
+  BOOST_CHECK(memcmp(buf1, ref_buf, ref_size) == 0);
+
+  /////////////////////////////////////////////////////////
+  // Copy conversion test
+
+  uint8_t *check_buf = buf1;
+  size_t check_size = size1;
+
+  memcpy(buf1, ref_buf, ref_size);
+  size1 = ref_size;
+
+  buf2.zero();
+  size2 = conv1(buf1, size1, buf2);
+
+  buf1.zero();
+  size1 = conv2(buf2, size2, buf1);
+
+  if (conv3)
+  {
+    buf2.zero();
+    size2 = conv3(buf1, size1, buf2);
+    check_buf = buf2;
+    check_size = size2;
+  }
+
+  BOOST_CHECK_EQUAL(check_size, ref_size);
+  BOOST_CHECK(memcmp(check_buf, ref_buf, ref_size) == 0);
+}
+
+BOOST_AUTO_TEST_CASE(bs_convert_2step)
+{
+  for (int i = 0; i < array_size(bs_types); i++)
+    for (int j = 0; j < array_size(bs_types); j++)
+      if (i != j)
+      {
+        BOOST_TEST_MESSAGE("Conversion " << 
+          bs_name(bs_types[i]) << "-" << 
+          bs_name(bs_types[j]));
+        bs_convert_test(bs_types[i], bs_types[j]);
+      }
+}
+
+BOOST_AUTO_TEST_CASE(bs_convert_3step)
+{
+  for (int i = 0; i < array_size(bs_types); i++)
+    for (int j = 0; j < array_size(bs_types); j++)
+      for (int k = 0; k < array_size(bs_types); k++)
+        if ((i != j) && (i != k) && (j != k))
+        {
+          BOOST_TEST_MESSAGE("Conversion " << 
+            bs_name(bs_types[i]) << "-" << 
+            bs_name(bs_types[j]) << "-" << 
+            bs_name(bs_types[k]));
+          bs_convert_test(bs_types[i], bs_types[j], bs_types[k]);
+        }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
