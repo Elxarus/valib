81391ea6b2cda624322b1a3b8e33de514f1a1bc4
valex <valex@localhost>
2010-09-08 05:11:35 +0400

    Doxygen: Chunk documented.

--- valib/chunk.h
+++ valib/chunk.h
@@ -1,5 +1,6 @@
-/*
-  Chunk
+/**************************************************************************//**
+  \class Chunk
+  \brief A part of audio data
 
   This structure is used to transfer audio data along with time stamps between
   Source, Filter and Sink objects.
@@ -7,77 +8,99 @@
   Format of the data is determined by output format of a Source or Filter.
   (see Filter::get_output() and Source::get_output())
 
-  /////////////////////////////////////////////////////////
-  // Data
-
-  uint8_t *rawdata;
+  \var uint8_t *Chunk::rawdata;
     Pointer to raw data buffer.
 
-  samples_t samples;
+  \var samples_t Chunk::samples;
     Pointer to samples buffer.
 
-  size_t size;
+  \var size_t Chunk::size;
     Size of the buffer. When raw data buffer is given, it means size fo the
     buffer in bytes. When samples buffer is given it means number of samples
     per channel.
 
-  bool sync;
+  \var bool Chunk::sync;
     When this flag is set, time stamp is set. Otherwise time should be ignored.
 
-  vtime_t time;
+  \var vtime_t Chunk::time;
     Time associated with the begnning of the data in the chunk.
 
     More specifically, this time should be applied to the first sync point in
     the chunk. For linear format this means the beginning of the first sample.
+
     For compressed formats such as AC3 or DTS first syncpoint is the beginning
     of the frame. I.e. if chunk contains the tail of a previous frame and a
     head of a next frame, time should be applied to the first sample of the new
     frame.
 
-  /////////////////////////////////////////////////////////
-  // Utilities
+  \fn Chunk::Chunk()
+    Constructs a dummy chunk with no data and no timestamp.
 
-  Chunk()
-    Constructs dummy chunk with no data and no timestamp.
+  \fn Chunk::Chunk(bool sync, vtime_t time):
+    \param sync Sync flag
+    \param time Time stamp
 
-  Chunk(bool sync, vtime_t time):
-    Empty chunk with time stamp.
+    Constructs an empty chunk with time stamp.
 
-  Chunk(samples_t samples, size_t size,
-        bool sync = false, vtime_t time = 0)
-    Linear format chunk constructor.
+  \fn Chunk::Chunk(samples_t samples, size_t size, bool sync = false, vtime_t time = 0)
+    \param samples Pointers to samples
+    \param size    Number of samples per channel
+    \param sync    Sync flag
+    \param time    Time stamp
 
-  Chunk(uint8_t *rawdata, size_t size,
-        bool sync = false, vtime_t time = 0)
-    Raw data format chunk constructor.
+    Constructs a chunk with linear format data.
 
-  void clear()
+  \fn Chunk::Chunk(uint8_t *rawdata, size_t size, bool sync = false, vtime_t time = 0)
+    \param rawdata Pointer to raw data buffer
+    \param size    Size of the buffer in bytes
+    \param sync    Sync flag
+    \param time    Time stamp
+
+    Constructs a chunk with raw data.
+
+  \fn void Chunk::clear()
     Drop data and time stamp. Chunk becomes dummy.
 
-  void set_linear(samples_t samples, size_t size,
-    bool sync = false, vtime_t time = 0)
-    Make linear format chunk.
+  \fn void Chunk::set_linear(samples_t samples, size_t size, bool sync = false, vtime_t time = 0)
+    \param samples Pointers to samples
+    \param size    Number of samples per channel
+    \param sync    Sync flag
+    \param time    Time stamp
 
-  void set_rawdata(uint8_t *rawdata, size_t size,
-    bool sync = false, vtime_t time = 0)
-    Make raw data chunk.
+    Fills the chunk with linear format data.
 
-  void set_sync(bool sync, vtime_t time)
+  \fn void Chunk::set_rawdata(uint8_t *rawdata, size_t size, bool sync = false, vtime_t time = 0)
+    \param rawdata Pointer to raw data buffer
+    \param size    Size of the buffer in bytes
+    \param sync    Sync flag
+    \param time    Time stamp
+
+    Fills the chunk with raw data.
+
+  \fn void Chunk::set_sync(bool sync, vtime_t time)
+    \param sync Sync flag
+    \param time Time stamp
+
     Set time stamp for the chunk.
 
-  bool is_dummy() const
+  \fn bool Chunk::is_dummy() const
     Check for dummy chunk (chunk that does not contain data or time stamp).
 
-  bool is_empty() const
+  \fn bool Chunk::is_empty() const
     Check for empty chunk (chunk that does not contain data).
 
-  void drop_rawdata(size_t drop_size)
-    Move raw data pointer ahead by drop_size bytes.
+  \fn void Chunk::drop_rawdata(size_t drop_size)
+    \param drop_size Size of data to drop.
 
-  void drop_samples(size_t drop_size)
+    Drops some data from the beginning of the chunk.
+    Moves raw data pointer ahead by drop_size bytes.
+
+  \fn void Chunk::drop_samples(size_t drop_size)
+    \param drop_size Size of data to drop.
+
+    Drops some data from the beginning of the chunk.
     Move raw sample pointers ahead by drop_size samples.
-
-*/
+******************************************************************************/
 
 #ifndef VALIB_CHUNK_H
 #define VALIB_CHUNK_H
