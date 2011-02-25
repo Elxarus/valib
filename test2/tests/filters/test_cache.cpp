fa612ebcc3bc414f036cc799e471ee3debf92e12
valex <valex@localhost>
2011-02-25 14:55:59 +0300

    test_cache: use SampleBufNoise; more tests

--- test2/tests/filters/test_cache.cpp
+++ test2/tests/filters/test_cache.cpp
@@ -5,6 +5,7 @@
 #include <boost/test/unit_test.hpp>
 #include "filters/cache.h"
 #include "source/generator.h"
+#include "../../noise_buf.h"
 
 static const int seed = 34098577;
 static const size_t buf_size = 1024;
@@ -72,11 +73,9 @@ BOOST_AUTO_TEST_CASE(get_samples)
   CacheFilter f(cache_size);
   BOOST_REQUIRE(f.open(spk));
 
-  size_t cache_samples = f.get_nsamples();
-  size_t chunk_size = round<size_t>(cache_samples / 1.3);
-
   Chunk noise_chunk;
-  NoiseGen noise(spk, seed, chunk_size * 10, chunk_size);
+  size_t chunk_size = round<size_t>(f.get_nsamples() / 1.3);
+  NoiseGen noise(spk, seed, chunk_size * 5, chunk_size);
   while (noise.get_chunk(noise_chunk))
   {
     f.process(Chunk(noise_chunk), Chunk());
@@ -85,31 +84,79 @@ BOOST_AUTO_TEST_CASE(get_samples)
   }
 }
 
-BOOST_AUTO_TEST_CASE(resize)
+BOOST_AUTO_TEST_CASE(resize_underfill)
 {
-  Chunk noise_chunk;
-
+  // Put less data than the cache size and resize the cache
+  // Compare the data put into the cache
   CacheFilter f(cache_size);
   BOOST_REQUIRE(f.open(spk));
 
-  // Make noise buffer
-  size_t cache_samples = f.get_nsamples();
-  NoiseGen noise(spk, seed, cache_samples, cache_samples);
-  BOOST_REQUIRE(noise.get_chunk(noise_chunk));
+  // Put noise into the buffer and compare data
+  size_t noise_samples = f.get_nsamples() / 4;
+  SampleBufNoise noise(spk.nch(), noise_samples, seed);
+  BOOST_REQUIRE(f.process(Chunk(noise.samples(), noise_samples), Chunk()));
 
-  // Put noise into the buffer
-  BOOST_REQUIRE(f.process(Chunk(noise_chunk), Chunk()));
+  vtime_t time = f.get_time() - noise_samples / spk.sample_rate;
+  compare_cached_samples(f, noise.samples(), noise_samples, time);
 
   // Shrink the cache and compare data
   f.set_size(cache_size / 2);
-  cache_samples = f.get_nsamples();
-  Samples buf(cache_samples);
-  vtime_t time = f.get_time() - cache_samples / spk.sample_rate;
-  compare_cached_samples(f, noise_chunk.samples + noise_chunk.size - cache_samples, cache_samples, time);
+  compare_cached_samples(f, noise.samples(), noise_samples, time);
 
   // Expand the cache and compare data
   f.set_size(cache_size);
-  compare_cached_samples(f, noise_chunk.samples + noise_chunk.size - cache_samples, cache_samples, time);
+  compare_cached_samples(f, noise.samples(), noise_samples, time);
+}
+
+BOOST_AUTO_TEST_CASE(resize_fill)
+{
+  // Put amount of data that matches the cache size and resize the cache
+  // Compare the data put into the cache
+  CacheFilter f(cache_size);
+  BOOST_REQUIRE(f.open(spk));
+
+  // Put noise into the buffer and compare data
+  size_t noise_samples = f.get_nsamples();
+  SampleBufNoise noise(spk.nch(), noise_samples, seed);
+  BOOST_REQUIRE(f.process(Chunk(noise.samples(), noise_samples), Chunk()));
+
+  vtime_t time = f.get_time() - noise_samples / spk.sample_rate;
+  compare_cached_samples(f, noise.samples(), noise_samples, time);
+
+  // Shrink the cache and compare data
+  f.set_size(cache_size / 2);
+  noise_samples = f.get_nsamples() / 2;
+  compare_cached_samples(f, noise.samples() + noise.nsamples() - noise_samples, noise_samples, time);
+
+  // Expand the cache and compare data
+  f.set_size(cache_size);
+  compare_cached_samples(f, noise.samples() + noise.nsamples() - noise_samples, noise_samples, time);
+}
+
+BOOST_AUTO_TEST_CASE(resize_overfill)
+{
+  // Put more data than the cache size and resize the cache
+  // Compare the data put into the cache
+  CacheFilter f(cache_size);
+  BOOST_REQUIRE(f.open(spk));
+
+  // Put noise into the buffer and compare data
+  size_t noise_samples = round<size_t>(f.get_nsamples() * 1.5);
+  SampleBufNoise noise(spk.nch(), noise_samples, seed);
+  BOOST_REQUIRE(f.process(Chunk(noise.samples(), noise_samples), Chunk()));
+
+  noise_samples = f.get_nsamples();
+  vtime_t time = f.get_time() - noise_samples / spk.sample_rate;
+  compare_cached_samples(f, noise.samples() + noise.nsamples() - noise_samples, noise_samples, time);
+
+  // Shrink the cache and compare data
+  f.set_size(cache_size / 2);
+  noise_samples = f.get_nsamples() / 2;
+  compare_cached_samples(f, noise.samples() + noise.nsamples() - noise_samples, noise_samples, time);
+
+  // Expand the cache and compare data
+  f.set_size(cache_size);
+  compare_cached_samples(f, noise.samples() + noise.nsamples() - noise_samples, noise_samples, time);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
