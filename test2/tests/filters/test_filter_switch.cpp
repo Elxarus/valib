b17d2b19d16b7f96f08cc602c311a2a83cc29d53
valex <valex@localhost>
2011-02-27 13:15:51 +0300

    FilterSwitch: new members add_filter(), remove_filter(), release_filter()

--- test2/tests/filters/test_filter_switch.cpp
+++ test2/tests/filters/test_filter_switch.cpp
@@ -20,18 +20,19 @@ protected:
   Speakers accept_spk;
 };
 
+static const Speakers spk1(FORMAT_LINEAR, MODE_MONO, 48000);
+static const Speakers spk2(FORMAT_LINEAR, MODE_STEREO, 48000);
+
 BOOST_AUTO_TEST_SUITE(filter_switch)
 
 BOOST_AUTO_TEST_CASE(constructor)
 {
   FilterSwitch f;
+  BOOST_CHECK_EQUAL(f.get_filters().size(), 0);
 }
 
-BOOST_AUTO_TEST_CASE(open)
+BOOST_AUTO_TEST_CASE(init_constructor)
 {
-  const Speakers spk1(FORMAT_LINEAR, MODE_MONO, 48000);
-  const Speakers spk2(FORMAT_LINEAR, MODE_STEREO, 48000);
-
   FilterSwitchMock f1(spk1);
   FilterSwitchMock f2(spk2);
 
@@ -39,15 +40,82 @@ BOOST_AUTO_TEST_CASE(open)
   list.push_back(&f1);
   list.push_back(&f2);
 
-  FilterSwitch filter_switch(list);
+  FilterSwitch f(list);
+  BOOST_CHECK_EQUAL(f.get_filters().size(), 2);
+  BOOST_CHECK_EQUAL(f.get_filters()[0], &f1);
+  BOOST_CHECK_EQUAL(f.get_filters()[1], &f2);
+  BOOST_CHECK(f.can_open(spk1));
+  BOOST_CHECK(f.can_open(spk2));
+}
 
-  filter_switch.open(spk1);
-  BOOST_CHECK(filter_switch.is_open());
+BOOST_AUTO_TEST_CASE(set_release)
+{
+  FilterSwitchMock f1(spk1);
+  FilterSwitchMock f2(spk2);
+
+  FilterSwitch::list_t list;
+  list.push_back(&f1);
+  list.push_back(&f2);
+
+  FilterSwitch f;
+  f.set_filters(list);
+  BOOST_CHECK_EQUAL(f.get_filters().size(), 2);
+  BOOST_CHECK_EQUAL(f.get_filters()[0], &f1);
+  BOOST_CHECK_EQUAL(f.get_filters()[1], &f2);
+  BOOST_CHECK(f.can_open(spk1));
+  BOOST_CHECK(f.can_open(spk2));
+
+  f.release_filters();
+  BOOST_CHECK_EQUAL(f.get_filters().size(), 0);
+  BOOST_CHECK(!f.can_open(spk1));
+  BOOST_CHECK(!f.can_open(spk2));
+}
+
+BOOST_AUTO_TEST_CASE(add_remove)
+{
+  FilterSwitchMock f1(spk1);
+  FilterSwitchMock f2(spk2);
+  FilterSwitch f;
+
+  f.add_filter(&f1);
+  BOOST_CHECK_EQUAL(f.get_filters().size(), 1);
+  BOOST_CHECK(f.can_open(spk1));
+  BOOST_CHECK(!f.can_open(spk2));
+
+  f.add_filter(&f2);
+  BOOST_CHECK_EQUAL(f.get_filters().size(), 2);
+  BOOST_CHECK(f.can_open(spk1));
+  BOOST_CHECK(f.can_open(spk2));
+
+  f.remove_filter(&f1);
+  BOOST_CHECK_EQUAL(f.get_filters().size(), 1);
+  BOOST_CHECK(!f.can_open(spk1));
+  BOOST_CHECK(f.can_open(spk2));
+
+  f.remove_filter(&f2);
+  BOOST_CHECK_EQUAL(f.get_filters().size(), 0);
+  BOOST_CHECK(!f.can_open(spk1));
+  BOOST_CHECK(!f.can_open(spk2));
+}
+
+BOOST_AUTO_TEST_CASE(open)
+{
+  FilterSwitchMock f1(spk1);
+  FilterSwitchMock f2(spk2);
+
+  FilterSwitch::list_t list;
+  list.push_back(&f1);
+  list.push_back(&f2);
+
+  FilterSwitch f(list);
+
+  f.open(spk1);
+  BOOST_CHECK(f.is_open());
   BOOST_CHECK(f1.is_open());
   BOOST_CHECK(!f2.is_open());
 
-  filter_switch.open(spk2);
-  BOOST_CHECK(filter_switch.is_open());
+  f.open(spk2);
+  BOOST_CHECK(f.is_open());
   BOOST_CHECK(!f1.is_open());
   BOOST_CHECK(f2.is_open());
 }
