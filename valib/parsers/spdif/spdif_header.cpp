6827f156bc1b33d942a89901c7a7a5c51731ec63
valex <valex@localhost>
2011-03-04 18:24:24 +0300

    SPDIFHeader: padded DTS format support

--- valib/parsers/spdif/spdif_header.cpp
+++ valib/parsers/spdif/spdif_header.cpp
@@ -28,21 +28,35 @@ inline static const HeaderParser *find_parser(int spdif_type)
 bool
 SPDIFHeader::parse_header(const uint8_t *hdr, HeaderInfo *hinfo) const
 {
-  if ((hdr[0] != 0x00) || (hdr[1] != 0x00) || (hdr[2]  != 0x00) || (hdr[3]  != 0x00) ||
-      (hdr[4] != 0x00) || (hdr[5] != 0x00) || (hdr[6]  != 0x00) || (hdr[7]  != 0x00) ||
-      (hdr[8] != 0x72) || (hdr[9] != 0xf8) || (hdr[10] != 0x1f) || (hdr[11] != 0x4e))
+  // DTS
+  const HeaderParser *parser = 0;
+  const uint8_t *payload = hdr;
+
+  if ((hdr[0] == 0x00) && (hdr[1] == 0x00) && (hdr[2]  == 0x00) && (hdr[3]  == 0x00) &&
+      (hdr[4] == 0x00) && (hdr[5] == 0x00) && (hdr[6]  == 0x00) && (hdr[7]  == 0x00) &&
+      (hdr[8] == 0x72) && (hdr[9] == 0xf8) && (hdr[10] == 0x1f) && (hdr[11] == 0x4e))
+  {
+    // Parse SPDIF header
+    const spdif_header_s *spdif_header = (spdif_header_s *)hdr;
+    payload = hdr + sizeof(spdif_header_s);
+    parser = find_parser(spdif_header->type);
+    if (!parser)
+      return false;
+  }
+  // DTS 16 bits low endian bitstream
+  else if (hdr[0] == 0xfe && hdr[1] == 0x7f &&
+           hdr[2] == 0x01 && hdr[3] == 0x80)
+    parser = &dts_header;
+  // DTS 14 bits low endian bitstream
+  else if (hdr[0] == 0xff && hdr[1] == 0x1f &&
+           hdr[2] == 0x00 && hdr[3] == 0xe8 &&
+          (hdr[4] & 0xf0) == 0xf0 && hdr[5] == 0x07)
+    parser = &dts_header;
+  else
     return false;
 
-  const spdif_header_s *header = (spdif_header_s *)hdr;
-  const uint8_t *subheader = hdr + sizeof(spdif_header_s);
   HeaderInfo subinfo;
-
-  const HeaderParser *parser = find_parser(header->type);
-
-  if (!parser)
-    return false;
-
-  if (!parser->parse_header(subheader, &subinfo))
+  if (!parser->parse_header(payload, &subinfo))
     return false;
 
   // SPDIF frame size equals to number of samples in contained frame
@@ -56,7 +70,7 @@ SPDIFHeader::parse_header(const uint8_t *hdr, HeaderInfo *hinfo) const
     hinfo->frame_size = subinfo.nsamples * 4;
     hinfo->scan_size = subinfo.nsamples * 4;
     hinfo->nsamples = subinfo.nsamples;
-    hinfo->spdif_type = header->type;
+    hinfo->spdif_type = 0;
   }
   return true;
 }
@@ -64,20 +78,33 @@ SPDIFHeader::parse_header(const uint8_t *hdr, HeaderInfo *hinfo) const
 bool
 SPDIFHeader::compare_headers(const uint8_t *hdr1, const uint8_t *hdr2) const
 {
-  if ((hdr1[0] != 0x00) || (hdr1[1] != 0x00) || (hdr1[2]  != 0x00) || (hdr1[3]  != 0x00) ||
-      (hdr1[4] != 0x00) || (hdr1[5] != 0x00) || (hdr1[6]  != 0x00) || (hdr1[7]  != 0x00) ||
-      (hdr1[8] != 0x72) || (hdr1[9] != 0xf8) || (hdr1[10] != 0x1f) || (hdr1[11] != 0x4e) ||
-
-      (hdr2[0] != 0x00) || (hdr2[1] != 0x00) || (hdr2[2]  != 0x00) || (hdr2[3]  != 0x00) ||
-      (hdr2[4] != 0x00) || (hdr2[5] != 0x00) || (hdr2[6]  != 0x00) || (hdr2[7]  != 0x00) ||
-      (hdr2[8] != 0x72) || (hdr2[9] != 0xf8) || (hdr2[10] != 0x1f) || (hdr2[11] != 0x4e))
-
-    return false;
-
-  const spdif_header_s *header = (spdif_header_s *)hdr1;
-  const HeaderParser *parser = find_parser(header->type);
-  if (parser)
-    return parser->compare_headers(hdr1 + sizeof(spdif_header_s), hdr2 + sizeof(spdif_header_s));
+  if ((hdr1[0] == 0x00) && (hdr1[1] == 0x00) && (hdr1[2]  == 0x00) && (hdr1[3]  == 0x00) &&
+      (hdr1[4] == 0x00) && (hdr1[5] == 0x00) && (hdr1[6]  == 0x00) && (hdr1[7]  == 0x00) &&
+      (hdr1[8] == 0x72) && (hdr1[9] == 0xf8) && (hdr1[10] == 0x1f) && (hdr1[11] == 0x4e) &&
+      (hdr2[0] == 0x00) && (hdr2[1] == 0x00) && (hdr2[2]  == 0x00) && (hdr2[3]  == 0x00) &&
+      (hdr2[4] == 0x00) && (hdr2[5] == 0x00) && (hdr2[6]  == 0x00) && (hdr2[7]  == 0x00) &&
+      (hdr2[8] == 0x72) && (hdr2[9] == 0xf8) && (hdr2[10] == 0x1f) && (hdr2[11] == 0x4e))
+  {
+    const spdif_header_s *header = (spdif_header_s *)hdr1;
+    const HeaderParser *parser = find_parser(header->type);
+    return parser && parser->compare_headers(
+      hdr1 + sizeof(spdif_header_s),
+      hdr2 + sizeof(spdif_header_s));
+  }
+  // DTS 16 bits low endian bitstream
+  else if (hdr1[0] == 0xfe && hdr1[1] == 0x7f &&
+           hdr1[2] == 0x01 && hdr1[3] == 0x80 &&
+           hdr2[0] == 0xfe && hdr2[1] == 0x7f &&
+           hdr2[2] == 0x01 && hdr2[3] == 0x80)
+    return dts_header.compare_headers(hdr1, hdr2);
+  // DTS 14 bits low endian bitstream
+  else if (hdr1[0] == 0xff && hdr1[1] == 0x1f &&
+           hdr1[2] == 0x00 && hdr1[3] == 0xe8 &&
+          (hdr1[4] & 0xf0) == 0xf0 && hdr1[5] == 0x07 &&
+           hdr2[0] == 0xff && hdr2[1] == 0x1f &&
+           hdr2[2] == 0x00 && hdr2[3] == 0xe8 &&
+          (hdr2[4] & 0xf0) == 0xf0 && hdr2[5] == 0x07)
+    return dts_header.compare_headers(hdr1, hdr2);
   else
     return false;
 }
@@ -103,6 +130,21 @@ SPDIFHeader::header_info(const uint8_t *hdr) const
     string result;
     result += string("Stream format: SPDIF/") + subinfo.spk.print() + string("\n");
     result += parser->header_info(subheader);
+    return result;
+  }
+  else if ((hdr[0] == 0xfe && hdr[1] == 0x7f &&
+            hdr[2] == 0x01 && hdr[3] == 0x80) ||
+           (hdr[0] == 0xff && hdr[1] == 0x1f &&
+            hdr[2] == 0x00 && hdr[3] == 0xe8 &&
+           (hdr[4] & 0xf0) == 0xf0 && hdr[5] == 0x07))
+  {
+    HeaderInfo hinfo;
+    if (!dts_header.parse_header(hdr, &hinfo))
+      return "Cannot parse substream header\n";
+
+    string result;
+    result += string("Stream format: Padded DTS");
+    result += dts_header.header_info(hdr);
     return result;
   }
   else
