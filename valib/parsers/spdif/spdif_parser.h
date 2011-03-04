de27ac8a13e3897aa921ba813de8eca04d4bd40e
valex <valex@localhost>
2011-03-04 14:11:28 +0300

    SPDIFParser: Documented, refactored.

--- valib/parsers/spdif/spdif_parser.h
+++ valib/parsers/spdif/spdif_parser.h
@@ -13,6 +13,37 @@
   \class SPDIFParser
   \brief Converts SPDIF stream to raw AC3/MPA/DTS stream.
 
+  Accepts only chunks with exactly one frame.
+
+  By default, it converts the stream from little endian format (SPDIF stream
+  always contains) to big endian. You can disable this behavior and keep little
+  enddian stream unchanged.
+
+  SPDIFWrapper produces two types of SPDIF stream: wrapped and padded DTS. This
+  class supports both.
+
+  Normally, the size of original frame is saved at SPDIF header, so the original
+  stream may be reconstructed prcisely. But padded DTS does not have SPDIF
+  header and frame size at the DTS header may be incorrect. Therefore, it is
+  impossible to determine the correct frame size and it remains unchanged.
+  I.e. SPDIFParser just passes the padded DTS without precise reconstruction of
+  an original stream.
+
+  The change of the contained stream is detected and output stream change
+  produced.
+
+  \fn bool SPDIFParser::get_big_endian() const
+    Returns big_endian option.
+
+  \fn void SPDIFParser::set_big_endian(bool big_endian)
+    \param big_endian Convert to big endian option.
+
+    Sets big_endian option. When set to true SPDIFParser converts the contained
+    stream to big endian format. Otherwise it keeps the little endian format
+    unchanged.
+
+  \fn HeaderInfo SPDIFParser::header_info() const
+    Returns the header info of the output stream.
 
 ******************************************************************************/
 
