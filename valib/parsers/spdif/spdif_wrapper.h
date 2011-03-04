19e6c04e902a166c470cc06d46448ec43b927c97
valex <valex@localhost>
2011-03-04 15:50:17 +0300

    SPDIFWrapper: documented. Rollback e666e7753004 changeset. Test updated.

--- valib/parsers/spdif/spdif_wrapper.h
+++ valib/parsers/spdif/spdif_wrapper.h
@@ -1,12 +1,7 @@
-/*
-  SPDIF wrapper class
-  Converts raw AC3/MPA/DTS stream to SPDIF stream.
-
-  Does raw stream output if the stream given cannot be spdifed (high-bitrate
-  DTS for example). Can convert DTS stream type to 14/16 bits. Supports padded
-  and wrapped SPDIF stream types. Can re-wrap SPDIF stream with conversion
-  between SPDIF stream types.
-*/
+/**************************************************************************//**
+  \file spdif_wrapper.h
+  \brief SPDIFWrapper: Converts raw AC3/MPA/DTS stream to SPDIF stream.
+******************************************************************************/
 
 #ifndef VALIB_SPDIF_WRAPPER_H
 #define VALIB_SPDIF_WRAPPER_H
@@ -21,6 +16,57 @@
 #define DTS_CONV_NONE    0
 #define DTS_CONV_16BIT   1
 #define DTS_CONV_14BIT   2
+
+/**************************************************************************//**
+  \class SPDIFWrapper
+  \brief Converts raw AC3/MPA/DTS stream to SPDIF stream.
+
+  Accepts only chunks with exactly one frame.
+
+  Does raw stream output if the stream given cannot be converted (high-bitrate
+  DTS for example). Can convert DTS stream type to 14/16 bits. Supports wrapped
+  SPDIF and padded-DTS output stream types.
+
+  Some data cannot be converted because it does not fit SPDIF bitrate
+  requirements. In this case filter switches to passthrough mode and output
+  the stream unchanged. We cannot determine the possibility of the conversion
+  in advance, and have to receive some data to decide. Passthrough mode allows
+  the client to handle the stream that cannot be converted (decode it for
+  example).
+
+  In conversion mode output format is FORMAT_SPDIF. In passthrough mode output
+  format equals to the format of the raw stream.
+
+  DTS may be handled in two ways:
+  \li It can be wrapped into SPDIF packets.
+  \li It can be padded with zeros to match SPDIF bitrate. No SPDIF header added.
+    This allows to pass a little higher bitrate DTS and such DTS streams are
+    widely used.
+
+  \c dts_mode option selects DTS output mode:
+  \li \c DTS_MODE_WRAPPED Wrap DTS into SPDIF packets. If DTS frame does not
+    fit SPDIF packet, filter switches to passthrough mode.
+  \li \c DTS_MODE_PADDED Pad each DTS frame with zeros to match SPDIF bitrate.
+    No SPDIF header added. If DTS does not fit SPDIF bitrate requirements,
+    filter switches to passthrough mode.
+  \li \c DTS_MODE_AUTO Wrap DTS into SPDIF packets when DTS frame fits SPDIF
+    packet. Pad DTS frame with zeros if it fits SPDIF bitrate requirements.
+    Switch to passthrough mode otherwise.
+
+  DTS frame size requirements
+  \li Wrapped mode: dts_frame_size <= spdif_frame_size - spdif_header_size
+  \li Padded mode dts_frame_size <= spdif_frame_size
+  Where spdif_frame_size = dts_frame_samples * 4
+
+  DTS stream format may be changed to 14bit or 16bit. \c dts_conv option
+  selects the conversion mode:
+  \li \c DTS_CONV_NONE Do not convert bitstream type.
+  \li \c DTS_CONV_16BIT Convert any bitstream type to 16bit.
+  \li \c DTS_CONV_14BIT Convert any bitstream type to 14bit. Note, that frame
+    size may grow and exceed the frame size requirements. Conversion is not
+    done in this case.
+
+******************************************************************************/
 
 class SPDIFWrapper : public SimpleFilter
 {
