dd2e4d4973dd775e3561c2f458f6ffdee042a8da
valex <valex@localhost>
2011-03-09 21:29:32 +0300

    SPDIFParser test: SPDIF/PaddedDTS test added

--- test2/tests/parsers/spdif/test_spdif_parser.cpp
+++ test2/tests/parsers/spdif/test_spdif_parser.cpp
@@ -3,11 +3,14 @@
 */
 
 #include <boost/test/unit_test.hpp>
+#include "filters/filter_graph.h"
 #include "parsers/spdif/spdif_header.h"
 #include "parsers/spdif/spdif_parser.h"
+#include "parsers/spdif/spdif_wrapper.h"
 #include "parsers/spdif/spdifable_header.h"
 #include "source/file_parser.h"
 #include "../../../suite.h"
+#include "dts_frame_resize.h"
 
 BOOST_AUTO_TEST_SUITE(spdif_parser)
 
@@ -28,6 +31,25 @@ BOOST_AUTO_TEST_CASE(parse)
 
   SPDIFParser spdif;
   compare(&f_spdif, &spdif, &f_raw, 0);
+}
+
+BOOST_AUTO_TEST_CASE(dts_spdif)
+{
+  // Test chain: FileParser -> (DTS) -> SPDIFWrapper -> (SPDIF/DTS) -> SPDIFParser
+  FileParser f;
+  f.open_probe("a.dts.03f.dts", &dts_header);
+  BOOST_REQUIRE(f.is_open());
+
+  SPDIFWrapper spdifer(DTS_MODE_PADDED);
+  SPDIFParser parser;
+
+  // Test chain: FileParser -> (DTS) -> DTSFrameResize
+  FileParser f_ref;
+  f_ref.open_probe("a.dts.03f.dts", &dts_header);
+  BOOST_REQUIRE(f_ref.is_open());
+  DTSFrameResize resize(2048);
+
+  compare(&f, &FilterChain(&spdifer, &parser), &f_ref, &resize);
 }
 
 BOOST_AUTO_TEST_CASE(streams_frames)
