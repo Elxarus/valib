b706e88c6c21c202903fd382a3ab8f0aea7e2992
valex <valex@localhost>
2011-11-27 01:02:53 +0400

    EAC3Header removed.

--- valib/parsers/eac3/eac3_header.h
+++ valib/parsers/eac3/eac3_header.h
@@ -22,26 +22,4 @@ protected:
   virtual SyncInfo build_syncinfo(const uint8_t *frame, size_t size, const FrameInfo &finfo) const;
 };
 
-///////////////////////////////////////////////////////////////////////////////
-
-class EAC3Header : public HeaderParser
-{
-public:
-  EAC3Header() {};
-
-  /////////////////////////////////////////////////////////
-  // HeaderParser overrides
-
-  virtual SyncTrie sync_trie() const;
-  virtual size_t   header_size()    const  { return 6;    }
-  virtual size_t   min_frame_size() const  { return 6;    }
-  virtual size_t   max_frame_size() const  { return 4096; }
-  virtual bool     can_parse(int format) const { return format == FORMAT_EAC3; };
-
-  virtual bool     parse_header(const uint8_t *hdr, HeaderInfo *hinfo = 0) const;
-  virtual bool     compare_headers(const uint8_t *hdr1, const uint8_t *hdr2) const;
-};
-
-extern const EAC3Header eac3_header;
-
 #endif
