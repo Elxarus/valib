df2ffec61f359078dcdc977b92b2e63dbfcc053e
valex <valex@localhost>
2012-01-09 01:35:04 +0400

    AACParser: info() method implemented.

--- valib/parsers/aac/aac_parser.cpp
+++ valib/parsers/aac/aac_parser.cpp
@@ -1,3 +1,5 @@
+#include <sstream>
+#include "../../bitstream.h"
 #include "../../buffer.h"
 #include "aac_parser.h"
 #include "../../../3rdparty/faad/neaacdec.h"
@@ -173,4 +175,64 @@ AACParser::process(Chunk &in, Chunk &out)
     }
   }
   return false;
+}
+
+static const char digit[16] =
+{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+
+static const int sampling_frequency_tbl[16] =
+{
+  96000, 88200, 64000, 48000, 44100, 32000,
+  24000, 22050, 16000, 12000, 11025, 8000,
+  0, 0, 0, 0
+};
+
+static const char *channel_configuration_tbl[16] =
+{
+  "",
+  "1 channel: C",
+  "2 channels: L R",
+  "3 channels: L C R",
+  "4 channels: L C R S",
+  "5 channels: L C R BL BR",
+  "6 channels: L C R BL BR LFE",
+  "8 channels: L C R SL SR BL BR LFE",
+  "","","","","","","",""
+};
+
+string
+AACParser::info() const
+{
+  if (spk.format != FORMAT_AAC_FRAME)
+    return string("No sync");
+
+  uint8_t *format_data = spk.format_data.get();
+  std::stringstream result;
+
+  result << "Format data:";
+  for (size_t i = 0; i < spk.data_size; i++)
+    result << ' ' << digit[format_data[i] >> 4] << digit[format_data[i] & 0xf];
+  result << "\n";
+
+  ReadBS bs(format_data, 0, spk.data_size * 8);
+  int object_type = bs.get(5);
+  int sampling_frequency_index = bs.get(4);
+  int sampling_frequency = sampling_frequency_tbl[sampling_frequency_index];
+  if (sampling_frequency_index == 15)
+    sampling_frequency = bs.get(24);
+  int channel_configuration = bs.get(4);
+
+  result << "Audio Specific Config:\n";
+  result << "  Object type: ";
+  switch (object_type)
+  {
+    case 1:  result << "AAC Main\n"; break;
+    case 2:  result << "AAC LC\n";   break;
+    case 3:  result << "AAC SSR\n";  break;
+    case 4:  result << "AAC LTP\n";  break;
+    default: result << object_type << "\n"; break;
+  }
+  result << "  Sampling frequency: " << sampling_frequency << "\n";
+  result << "  Channel configuration: " << channel_configuration_tbl[channel_configuration] << "\n\n";
+  return result.str();
 }
