f71bb5164aaff307ff29cc42409efbcaa94f9fcd
valex <valex@localhost>
2012-01-12 21:25:20 +0400

    SPDIFFrameParser test: MPA samples added.

--- valib/parsers/spdif/spdifable_header.h
+++ valib/parsers/spdif/spdifable_header.h
@@ -26,14 +26,16 @@ public:
       // AC3
       case 1: return &ac3;
       // MPA
-      case 4:
-      case 5:
-      case 8:
-      case 9: return &mpa;
+      case 4: // MPEG1 Layer I
+      case 5: // MPEG1 Layer II/Layer III
+      case 8: // MPEG2 LSF Layer I
+      case 9: // MPEG2 LSF Layer II/Layer III
+        return &mpa;
       // DTS
-      case 11:
-      case 12:
-      case 13: return &dts;
+      case 11: // 512 samples/frame
+      case 12: // 1024 samples/frame
+      case 13: // 2048 samples/frame
+        return &dts;
     }
     return 0;
   }
