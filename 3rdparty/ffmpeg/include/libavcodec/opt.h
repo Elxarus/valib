2a1360b3ef359e45dec70a12fbb32e259d7fd51a
valex <valex@localhost>
2012-05-05 20:54:52 +0400

    ffmpeg updated (aac, dts, vorbis, ape and alac enabled).

--- 3rdparty/ffmpeg/include/libavcodec/opt.h
+++ 3rdparty/ffmpeg/include/libavcodec/opt.h
@@ -25,10 +25,10 @@
 #ifndef AVCODEC_OPT_H
 #define AVCODEC_OPT_H
 
-#include "../libavcodec/version.h"
+#include "libavcodec/version.h"
 
 #if FF_API_OPT_H
-#include "../libavutil/opt.h"
+#include "libavutil/opt.h"
 #endif
 
 #endif /* AVCODEC_OPT_H */
