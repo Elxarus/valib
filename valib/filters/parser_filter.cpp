b081c7d19546ab47a1545e5137fed3bba162ec8a
valex <valex@localhost>
2012-05-20 01:18:03 +0400

    ParserFilter: do not mix frame parsers together. Use only complementary filter for a frame parser currently used.

--- valib/filters/parser_filter.cpp
+++ valib/filters/parser_filter.cpp
@@ -1,32 +1,58 @@
 #include "parser_filter.h"
 
-void
-ParserFilter::add(FrameParser *new_header, Filter *new_filter)
+ParserFilter::ParserFilter()
+{}
+
+ParserFilter::ParserFilter(FrameParser *frame_parser_, Filter *filter_)
 {
-  parser.add_parser(new_header);
-  filter.add_filter(new_filter);
-  sync.set_parser(&parser);
+  add(frame_parser_, filter_);
 }
 
 void
-ParserFilter::add(FrameParser *new_header)
+ParserFilter::add(FrameParser *frame_parser_, Filter *filter_)
 {
-  parser.add_parser(new_header);
-  sync.set_parser(&parser);
+  list.push_back(Entry(frame_parser_, filter_));
 }
 
 void
-ParserFilter::add(Filter *new_filter)
+ParserFilter::add(Filter *filter_)
 {
-  filter.add_filter(new_filter);
+  list.push_back(Entry(0, filter_));
 }
 
 void
 ParserFilter::release()
 {
-  parser.release_parsers();
-  filter.release_filters();
-  sync.set_parser(&parser);
+  list.clear();
+}
+
+/////////////////////////////////////////////////////////
+// Filter overrides
+
+bool
+ParserFilter::can_open(Speakers spk) const
+{
+  for (size_t i = 0; i < list.size(); i++)
+    if ((list[i].frame_parser && list[i].frame_parser->can_parse(spk.format)) ||
+        (list[i].filter && list[i].filter->can_open(spk)))
+      return true;
+  return false;
+}
+
+bool
+ParserFilter::open(Speakers spk)
+{
+  for (size_t i = 0; i < list.size(); i++)
+    if ((list[i].frame_parser && list[i].frame_parser->can_parse(spk.format)) ||
+        (list[i].filter && list[i].filter->can_open(spk)))
+    {
+      frame_parser = list[i].frame_parser;
+      filter = list[i].filter;
+      sync.set_parser(frame_parser);
+      return FilterGraph::open(spk);
+    }
+
+  return false;
 }
 
 /////////////////////////////////////////////////////////
@@ -38,19 +64,12 @@ ParserFilter::next_id(int id, Speakers spk) const
   switch (id)
   {
     case node_start:
-      if (sync.can_open(spk))
+      if (frame_parser)
         return node_sync;
-
-      if (filter.can_open(spk))
-        return node_filter;
-
-      return node_err;
+      return node_filter;
 
     case node_sync:
-      if (filter.can_open(spk))
-        return node_filter;
-
-      return node_err;
+      return node_filter;
   
     case node_filter:
       return node_end;
@@ -64,7 +83,7 @@ ParserFilter::init_filter(int id, Speakers spk)
   switch (id)
   {
     case node_sync:   return &sync;
-    case node_filter: return &filter;
+    case node_filter: return filter;
   }
   return 0;
 }
