4492e410ef4a4d27374ae59c79c7b91345e43386
valex <valex@localhost>
2012-06-13 13:29:16 +0400

    FfmpegDecoder: request higher quality output format.

--- test2/tests/parsers/eac3/test_eac3_parser.cpp
+++ test2/tests/parsers/eac3/test_eac3_parser.cpp
@@ -25,12 +25,16 @@ BOOST_AUTO_TEST_CASE(streams_frames)
   EAC3FrameParser frame_parser;
   f.open_probe("test.eac3.03f.eac3", &frame_parser);
   BOOST_REQUIRE(f.is_open());
+  BOOST_CHECK_EQUAL(f.get_output(), Speakers(FORMAT_EAC3, MODE_5_1, 48000));
 
   EAC3Parser parser;
   parser.open(f.get_output());
   BOOST_CHECK(parser.is_open());
 
   check_streams_chunks(&f, &parser, 1, 819);
+
+  // Decoder's output must be floating point
+  BOOST_CHECK_EQUAL(parser.get_output(), Speakers(FORMAT_PCMFLOAT, MODE_5_1, 48000));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
