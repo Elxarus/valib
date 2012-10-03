daf15e8ac9e930d5a1ca1292eba0b7e2b9340600
valex <valex@localhost>
2012-10-03 12:05:33 +0400

    SinkFilter: split .h to .h and .cpp

--- valib/sink/sink_filter.h
+++ valib/sink/sink_filter.h
@@ -16,149 +16,30 @@ protected:
   Filter *filter;
 
 public:
-  struct Error : public Sink::Error {};
+  SinkFilter();
+  SinkFilter(Sink *sink, Filter *filter);
 
-  SinkFilter():
-  sink(0), filter(0)
-  {}
-
-  SinkFilter(Sink *sink_, Filter *filter_):
-  sink(0), filter(0)
-  { set(sink_, filter_); }
-
-  void set(Sink *sink_, Filter *filter_)
-  {
-    release();
-
-    if (sink_ && filter_)
-    {
-      if (filter_->is_open())
-      {
-        Speakers filter_spk = filter_->get_output();
-        if (!filter_spk.is_unknown())
-          if (!sink_->is_open() || sink_->get_input() != filter_spk)
-            sink_->open_throw(filter_spk);
-      }
-    }
-
-    sink = sink_;
-    filter = filter_;
-  }
-
-  void release()
-  {
-    sink = 0;
-    filter = 0;
-  }
+  void set(Sink *sink, Filter *filter);
+  void release();
 
   Sink   *get_sink()   const { return sink;   }
   Filter *get_filter() const { return filter; }
 
   /////////////////////////////////////////////////////////
-  // Open/close the filter
+  // Open/close the sink
 
-  virtual bool can_open(Speakers spk) const
-  {
-    if (!sink) return false;
-    if (!filter) return sink->can_open(spk);
-    return filter->can_open(spk);
-  }
-
-  virtual bool open(Speakers spk)
-  {
-    if (!sink) return false;
-    if (!filter) return sink->open(spk);
-
-    if (!filter->open(spk))
-      return false;
-
-    Speakers filter_spk = filter->get_output();
-    if (filter_spk.is_unknown())
-      return true;
-
-    return sink->open(filter_spk);
-  }
-
-  virtual void close()
-  {
-    if (!sink) return;
-
-    sink->close();
-    if (filter)
-      filter->close();
-  }
+  virtual bool can_open(Speakers spk) const;
+  virtual bool open(Speakers spk);
+  virtual void close();
+  virtual bool is_open() const;
+  virtual Speakers get_input() const;
 
   /////////////////////////////////////////////////////////
   // Processing
 
-  virtual void reset()
-  {
-    if (!sink) return;
-
-    sink->reset();
-    if (filter)
-      filter->reset();
-  }
-
-  virtual void process(const Chunk &in)
-  {
-    if (!sink) return;
-    if (!filter)
-    {
-      sink->process(in);
-      return;
-    }
-
-    Chunk non_const_in = in;
-    Chunk out;
-    while (filter->process(non_const_in, out))
-    {
-      if (filter->new_stream())
-      {
-        sink->flush();
-        if (!sink->open(filter->get_output()))
-          THROW(Error());
-      }
-      sink->process(out);
-    }
-  }
-
-  virtual void flush()
-  {
-    if (!sink) return;
-    if (!filter)
-    {
-      sink->flush();
-      return;
-    }
-
-    Chunk out;
-    while (filter->flush(out))
-    {
-      if (filter->new_stream())
-      {
-        sink->flush();
-        if (!sink->open(filter->get_output()))
-          THROW(Error());
-      }
-      sink->process(out);
-    }
-    sink->flush();
-  }
-
-  virtual bool is_open() const
-  {
-    if (!sink) return false;
-    if (filter) return filter->is_open() && sink->is_open();
-    return sink->is_open();
-  }
-
-  virtual Speakers get_input() const
-  {
-    if (!sink) return spk_unknown;
-    if (!filter) return sink->get_input();
-    return filter->get_input();
-  }
+  virtual void reset();
+  virtual void process(const Chunk &in);
+  virtual void flush();
 };
 
 #endif
