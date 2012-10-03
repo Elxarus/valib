ccb0d44ee609adad9e5bebd9c21fd36b1843dafb
valex <valex@localhost>
2012-10-03 19:21:15 +0400

    WAVSink: documentation

--- valib/sink/sink_wav.h
+++ valib/sink/sink_wav.h
@@ -1,8 +1,79 @@
+/**************************************************************************//**
+  \file sink_wav.h
+  \brief WAVSink class
+******************************************************************************/
+
 #ifndef VALIB_SINK_WAV
 #define VALIB_SINK_WAV
 
 #include "../sink.h"
 #include "../auto_file.h"
+
+/**************************************************************************//**
+  \class WAVSink
+  \brief Writes incoming data to WAV file.
+
+  File must be open with open_file() funtion before Sink interface may be used.
+  Before open_file() call sink refuses to open at all (can_open() returns
+  false).
+
+  After file is open, sink may be open with any format that is convertable to
+  WAVEFORMATEX with spk2wfe() function.
+
+  flush(), close() and close_file() are optional, WAVSink closes the file
+  correctly on destruction. But the resulting wav file becomes valid only after
+  closing the file.
+
+  Usage example:
+  \code
+  WAVSink sink;
+  if (!sink.open(file_name))
+  {
+    // Error: file cannot be open
+    return;
+  }
+  sink.open_throw(spk);
+
+  // Write data to WAV file
+  while (get_data(chunk))
+    sink.process(chunk);
+
+  // Close
+  sink.flush(); // optional
+  sink.close(); // optional
+  sink.close_file(); // optional
+  \endcode
+
+  \fn WAVSink::WAVSink();
+    Default constructor. Does not open file or sink.
+
+  \fn WAVSink::WAVSink(const char *file_name);
+    This constructor opens the file specified. Does not throw on error. You
+    should check is_file_open() explicitly before using the sink.
+
+  \fn WAVSink::~WAVSink()
+    Closes the file if nessesary.
+
+  \fn bool WAVSink::open_file(const char *file_name);
+    \paran file_name File name to write to.
+
+    Open the file specified for writing. Does not add .wav extension, it must
+    be done explicitly if required.
+
+    Does not opens the sink itself. It must be done explicitly with open() call.
+
+    Returns true on success and false otherwise.
+
+  \fn void WAVSink::close_file();
+    Closes the file currently open.
+
+  \fn bool WAVSink::is_file_open() const;
+    Returns true if file is currently open.
+
+  \fn string WAVSink::filename() const;
+    Returns the name of the file currently open.
+
+******************************************************************************/
 
 class WAVSink : public SimpleSink
 {
